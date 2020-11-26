de2e511fe500ea9a6b7a30dc0379fa8d38aa5c13
KangLin <kl222@126.com>
2020-11-26 11:45:23 +0800

    Add m_pView->setAttribute(Qt::WA_DeleteOnClose);

--- Viewer/Connecter.cpp
+++ Viewer/Connecter.cpp
@@ -2,7 +2,9 @@
 
 CConnecter::CConnecter(QObject *parent) : QObject(parent),
     m_pView(new CFrmViewer())
-{}
+{
+    m_pView->setAttribute(Qt::WA_DeleteOnClose);
+}
 
 CFrmViewer* CConnecter::GetViewer()
 {
