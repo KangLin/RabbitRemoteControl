87332fc098976e8c01e7f81d0dd01c2907a2ac79
KangLin <kl222@126.com>
2021-06-10 14:17:41 +0800

    LibSSH: modify translation

--- Viewer/Plugins/LibSSH/Resource/Translations/LibSSH_zh_CN.ts
+++ Viewer/Plugins/LibSSH/Resource/Translations/LibSSH_zh_CN.ts
@@ -2,39 +2,6 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
-    <name>CConnectSSH</name>
-    <message>
-        <source>Host key for server changed : server&apos;s one is now :
-</source>
-        <translation type="vanished">服务器主机键改变：现在服务器键是：&apos;s</translation>
-    </message>
-    <message>
-        <source>For security reason, connection will be stopped
-</source>
-        <translation type="vanished">因为安全原因，连接将停止</translation>
-    </message>
-    <message>
-        <source>The host key for this server was not found but an other type of key exists.
-</source>
-        <translation type="vanished">这个服务器的主机键没有查到，但是有期它键存在。</translation>
-    </message>
-    <message>
-        <source>Could not find known host file. If you accept the host key here,
-</source>
-        <translation type="vanished">不能查到服务器主机键文件。你是否接受这个主机键。</translation>
-    </message>
-    <message>
-        <source>the file will be automatically created.
-</source>
-        <translation type="vanished">这个文件将自动建立。</translation>
-    </message>
-    <message>
-        <source>The server is unknown. This new key will be written on disk for further usage.
-</source>
-        <translation type="vanished">这台服务器是未知的，这个新键将写入硬盘，便于将来使用。</translation>
-    </message>
-</context>
-<context>
     <name>CDlgSettingsSSH</name>
     <message>
         <location filename="../../DlgSettingsSSH.ui" line="14"/>
@@ -74,12 +41,12 @@
     <message>
         <location filename="../../DlgSettingsSSH.ui" line="108"/>
         <source>Capture file:</source>
-        <translation type="unfinished"></translation>
+        <translation>捕获文件:</translation>
     </message>
     <message>
         <location filename="../../DlgSettingsSSH.ui" line="118"/>
         <source>Brower(&amp;B)</source>
-        <translation type="unfinished"></translation>
+        <translation>浏览(&amp;B)</translation>
     </message>
     <message>
         <location filename="../../DlgSettingsSSH.ui" line="146"/>
@@ -93,12 +60,12 @@
     </message>
     <message>
         <source>Appearance</source>
-        <translation type="vanished">显示</translation>
+        <translation>显示</translation>
     </message>
     <message>
         <location filename="../../DlgSettingsSSH.cpp" line="64"/>
         <source>Capture file</source>
-        <translation type="unfinished"></translation>
+        <translation>捕获文件</translation>
     </message>
 </context>
 <context>
--- Viewer/Plugins/LibSSH/Resource/Translations/LibSSH_zh_TW.ts
+++ Viewer/Plugins/LibSSH/Resource/Translations/LibSSH_zh_TW.ts
@@ -1,39 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_TW">
-<context>
-    <name>CConnectSSH</name>
-    <message>
-        <source>Host key for server changed : server&apos;s one is now :
-</source>
-        <translation type="vanished">服務器主機鍵改變：現在服務器鍵是：&apos;s</translation>
-    </message>
-    <message>
-        <source>For security reason, connection will be stopped
-</source>
-        <translation type="vanished">因為安全原因，連接將停止</translation>
-    </message>
-    <message>
-        <source>The host key for this server was not found but an other type of key exists.
-</source>
-        <translation type="vanished">這個服務器的主機鍵沒有查到，但是有期它鍵存在。</translation>
-    </message>
-    <message>
-        <source>Could not find known host file. If you accept the host key here,
-</source>
-        <translation type="vanished">不能查到服務器主機鍵文件。妳是否接受這個主機鍵。</translation>
-    </message>
-    <message>
-        <source>the file will be automatically created.
-</source>
-        <translation type="vanished">這個文件將自動建立。</translation>
-    </message>
-    <message>
-        <source>The server is unknown. This new key will be written on disk for further usage.
-</source>
-        <translation type="vanished">這臺服務器是未知的，這個新鍵將寫入硬盤，便於將來使用。</translation>
-    </message>
-</context>
+<TS version="2.1" language="zh_CN">
 <context>
     <name>CDlgSettingsSSH</name>
     <message>
@@ -74,12 +41,12 @@
     <message>
         <location filename="../../DlgSettingsSSH.ui" line="108"/>
         <source>Capture file:</source>
-        <translation type="unfinished"></translation>
+        <translation>捕獲文件:</translation>
     </message>
     <message>
         <location filename="../../DlgSettingsSSH.ui" line="118"/>
         <source>Brower(&amp;B)</source>
-        <translation type="unfinished"></translation>
+        <translation>瀏覽(&amp;B)</translation>
     </message>
     <message>
         <location filename="../../DlgSettingsSSH.ui" line="146"/>
@@ -93,12 +60,12 @@
     </message>
     <message>
         <source>Appearance</source>
-        <translation type="vanished">顯示</translation>
+        <translation>顯示</translation>
     </message>
     <message>
         <location filename="../../DlgSettingsSSH.cpp" line="64"/>
         <source>Capture file</source>
-        <translation type="unfinished"></translation>
+        <translation>捕獲文件</translation>
     </message>
 </context>
 <context>
@@ -111,7 +78,7 @@
     <message>
         <location filename="../../PluginFactoryLibSSH.cpp" line="44"/>
         <source>SSH: you can remotely execute programs, transfer files, use a secure and transparent tunnel, manage public keys and much more ...</source>
-        <translation>SSH： 妳能用安全傳輸隧道遠程執行程序、傳輸文件；管理公共鍵等……</translation>
+        <translation>SSH： 你能用安全傳輸隧道遠程執行程序、傳輸文件；管理公共鍵等……</translation>
     </message>
 </context>
 </TS>
