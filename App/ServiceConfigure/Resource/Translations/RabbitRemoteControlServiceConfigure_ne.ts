0f37e00705d8c60f96c6c0792f7821ac9bcf2a39
Kang Lin <kl222@126.com>
2022-02-22 12:00:02 +0800

    App::Service: move folder

---
+++ App/ServiceConfigure/Resource/Translations/RabbitRemoteControlServiceConfigure_ne.ts
@@ -0,0 +1,109 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ne_NP">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../MainWindow.ui" line="14"/>
+        <source>Rabbit remote control service configure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.ui" line="26"/>
+        <source>Tab 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.ui" line="31"/>
+        <source>Tab 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.ui" line="54"/>
+        <source>Save(&amp;S)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.ui" line="61"/>
+        <source>Cancel(&amp;C)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.ui" line="80"/>
+        <source>Service</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.ui" line="88"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.ui" line="94"/>
+        <source>View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.ui" line="98"/>
+        <source>Style</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.ui" line="135"/>
+        <location filename="../../MainWindow.cpp" line="97"/>
+        <source>Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.ui" line="138"/>
+        <location filename="../../MainWindow.ui" line="141"/>
+        <location filename="../../MainWindow.ui" line="144"/>
+        <location filename="../../MainWindow.cpp" line="98"/>
+        <source>Start service</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.ui" line="149"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.ui" line="154"/>
+        <source>Default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.ui" line="163"/>
+        <source>Open Configure folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.ui" line="166"/>
+        <source>Open configure folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.cpp" line="104"/>
+        <source>Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.cpp" line="105"/>
+        <source>Stop service</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../MainConfigure.cpp" line="54"/>
+        <source>Rabbit remote control service configure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainConfigure.cpp" line="55"/>
+        <source>Kang Lin studio</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
