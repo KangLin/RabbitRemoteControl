dd21aeab80d37a8abbbb9d71cdc5a1941ca7cffb
Kang Lin <kl222@126.com>
2025-03-30 09:42:00 +0800

    Modify vcpkg.json

--- App/Client/mainwindow.cpp
+++ App/Client/mainwindow.cpp
@@ -520,10 +520,11 @@ void MainWindow::slotOpenFile(const QString& szFile, bool bOpenSettings)
 
 void MainWindow::on_actionOpenRRCFile_triggered()
 {
-    QString szFile = QFileDialog::getOpenFileName(this,
-                                                  tr("Open rabbit remote control file"),
-                                                  RabbitCommon::CDir::Instance()->GetDirUserData(),
-                                                  tr("Rabbit remote control Files (*.rrc);;All files(*.*)"));
+    QString szFile = QFileDialog::getOpenFileName(
+        this,
+        tr("Open rabbit remote control file"),
+        RabbitCommon::CDir::Instance()->GetDirUserData(),
+        tr("Rabbit remote control Files (*.rrc);;All files(*.*)"));
     if(szFile.isEmpty()) return;
 
     CConnecter* p = m_Client.LoadConnecter(szFile);
