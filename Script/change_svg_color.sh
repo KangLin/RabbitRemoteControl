cb726974669719c708148cda9229aad423f99ba4
Kang Lin <kl222@126.com>
2025-07-18 13:48:28 +0800

    App: add file transfer and file transfer list icon

--- Script/change_svg_color.sh
+++ Script/change_svg_color.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
-# Author: Kang Lin <kl222@126.com>
 
-#See: https://blog.csdn.net/alwaysbefine/article/details/114187380
-set -x
 set -e
-#set -v
 
-if [ $# -lt 3 ]; then
+if [ $# -lt 4 ]; then
     echo "Input parameters[$#] is fail. "
     echo "Please using:"
-    echo "$0 old_color new_color path"
+    echo "$0 source_path \"source_color\" destination_path \"destination_color\""
+	echo "ag:"
+	echo "$0 App/Client/Resource/icons/rabbit-green/svg \"#00ff00\" App/Client/Resource/icons/rabbit-white/svg \"#ffffff\""
+    echo "$0 App/Client/Resource/icons/rabbit-red/svg \"#ff0000\" App/Client/Resource/icons/rabbit-white/svg \"#ffffff\""
     exit
 fi
 
-sed -i "s/fill=\"$1\"/fill=\"$2\"/g" `grep "fill=\"$1\"" -rl $3`
+source_path=$1
+source_color=$2
+destination_path=$3
+destination_color=$4
+
+if [ ! -d $destination_path ]; then
+    mkdir -p $destination_path
+fi
+cp -rf $source_path/* $destination_path/ 
+sed -i "s/fill=\"$source_color\"/fill=\"$destination_color\"/g" `grep "fill=\"$source_color\"" -rl $destination_path`
