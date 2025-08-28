5e6a6b6be469ef0aed381dff2ffbc582311faeaf
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2025-08-28 09:20:12 -0300

    Update RabbitRemoteControl.sh
    Update to reflect the latest build sent by mail for testing in 2025-08-28 09:12AM (BRT).

--- share/applications/RabbitRemoteControl.sh
+++ share/applications/RabbitRemoteControl.sh
@@ -7,17 +7,23 @@ PRONAME=$(readlink -f $0)
 #获取文件运行的当前目录
 INSTALL_PATH=$(readlink -f $(dirname $(dirname $PRONAME)))
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  xattr -r -d com.apple.quarantine "$INSTALL_PATH"
-  DYLD_FRAMEWORK_PATH=$HOMEBREW_PREFIX/lib
-  eval "$(/usr/local/bin/brew shellenv)"
-  export DYLD_FRAMEWORK_PATH=$DYLD_FRAMEWORK_PATH:$HOMEBREW_PREFIX/lib
-  echo "DYLD_FRAMEWORK_PATH:$DYLD_FRAMEWORK_PATH"
-fi
-
 echo "INSTALL_PATH:$INSTALL_PATH"
-export LD_LIBRARY_PATH=$INSTALL_PATH/bin:$INSTALL_PATH/lib:$HOMEBREW_PREFIX/lib:$INSTALL_PATH/lib/`uname -m`-linux-gnu:$INSTALL_PATH/plugins/Client:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$INSTALL_PATH/bin:$INSTALL_PATH/lib:$INSTALL_PATH/lib/`uname -m`-linux-gnu:$INSTALL_PATH/plugins/Client:$LD_LIBRARY_PATH
 echo "LD_LIBRARY_PATH:$LD_LIBRARY_PATH"
-export DYLD_LIBRARY_PATH=$INSTALL_PATH/lib
-echo "DYLD_LIBRARY_PATH:$DYLD_LIBRARY_PATH"
-$INSTALL_PATH/bin/RabbitRemoteControlApp $*
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  if [[ -f "$INSTALL_PATH/MacOS/RabbitRemoteControlApp" ]]; then
+    # Production
+    $INSTALL_PATH/MacOS/RabbitRemoteControlApp $*
+  else
+    # Development
+    xattr -r -d com.apple.quarantine "$INSTALL_PATH"
+    DYLD_FRAMEWORK_PATH=$INSTALL_PATH/FrameWorks:$HOMEBREW_PREFIX/lib
+    eval "$(/usr/local/bin/brew shellenv)"
+    export DYLD_FRAMEWORK_PATH=$DYLD_FRAMEWORK_PATH:$HOMEBREW_PREFIX/lib
+    echo "DYLD_FRAMEWORK_PATH:$DYLD_FRAMEWORK_PATH"
+    $INSTALL_PATH/bin/RabbitRemoteControlApp $*
+  fi
+else
+  $INSTALL_PATH/bin/RabbitRemoteControlApp $*
+fi
