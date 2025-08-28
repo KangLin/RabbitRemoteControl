3149c0adc36e20f05dcb6e79f1035d2ccbad624d
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2025-08-28 09:30:13 -0300

    Update RabbitRemoteControl.sh

--- share/applications/RabbitRemoteControl.sh
+++ share/applications/RabbitRemoteControl.sh
@@ -18,7 +18,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   else
     # Development
     xattr -r -d com.apple.quarantine "$INSTALL_PATH"
-    DYLD_FRAMEWORK_PATH=$INSTALL_PATH/FrameWorks:$HOMEBREW_PREFIX/lib
+    DYLD_FRAMEWORK_PATH=$HOMEBREW_PREFIX/lib
     eval "$(/usr/local/bin/brew shellenv)"
     export DYLD_FRAMEWORK_PATH=$DYLD_FRAMEWORK_PATH:$HOMEBREW_PREFIX/lib
     echo "DYLD_FRAMEWORK_PATH:$DYLD_FRAMEWORK_PATH"
