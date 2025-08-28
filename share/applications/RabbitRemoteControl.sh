f29cd0a95fc9619f917f0033f1a341dfbffd77c4
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2025-08-28 18:59:25 -0300

    Update RabbitRemoteControl.sh

--- share/applications/RabbitRemoteControl.sh
+++ share/applications/RabbitRemoteControl.sh
@@ -9,22 +9,26 @@ INSTALL_PATH=$(readlink -f $(dirname $(dirname $PRONAME)))
 
 echo "INSTALL_PATH:$INSTALL_PATH"
 export LD_LIBRARY_PATH=$INSTALL_PATH/bin:$INSTALL_PATH/lib:$INSTALL_PATH/lib/`uname -m`-linux-gnu:$INSTALL_PATH/plugins/Client:$LD_LIBRARY_PATH
-echo "LD_LIBRARY_PATH:$LD_LIBRARY_PATH"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
+  xattr -r -d com.apple.quarantine "$INSTALL_PATH"
   if [[ -f "$INSTALL_PATH/MacOS/RabbitRemoteControlApp" ]]; then
     # Production
+    echo "LD_LIBRARY_PATH:$LD_LIBRARY_PATH"
     $INSTALL_PATH/MacOS/RabbitRemoteControlApp $*
   else
     # Development
-    xattr -r -d com.apple.quarantine "$INSTALL_PATH"
     eval "$(/opt/homebrew/bin/brew shellenv)"
+    LD_LIBRARY_PATH=$HOMEBREW_PREFIX/lib:$LD_LIBRARY_PATH
     DYLD_FRAMEWORK_PATH=$HOMEBREW_PREFIX/lib
     eval "$(/usr/local/bin/brew shellenv)"
     export DYLD_FRAMEWORK_PATH=$DYLD_FRAMEWORK_PATH:$HOMEBREW_PREFIX/lib
+    export LD_LIBRARY_PATH=$HOMEBREW_PREFIX/lib:$LD_LIBRARY_PATH
+    echo "LD_LIBRARY_PATH:$LD_LIBRARY_PATH"
     echo "DYLD_FRAMEWORK_PATH:$DYLD_FRAMEWORK_PATH"
     $INSTALL_PATH/bin/RabbitRemoteControlApp $*
   fi
 else
+  echo "LD_LIBRARY_PATH:$LD_LIBRARY_PATH"
   $INSTALL_PATH/bin/RabbitRemoteControlApp $*
 fi
