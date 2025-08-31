0cc62d7dbc13e378b820eb500d88d22806ebaade
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2025-08-30 23:49:04 -0300

    Update update_translations.sh

--- Script/update_translations.sh
+++ Script/update_translations.sh
@@ -10,15 +10,23 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 # Remove the '/Script' component from the end of the directory path.
 SCRIPT_DIR="${SCRIPT_DIR%/Script}"
 
+# Only update TS files of a specific language (e.g., pt_BR) if the first command-line argument is set.
+if [ -n "$1" ]; then
+	TS_LANG=$1
+fi
+
 echo "Starting recursive search for *.ts files in $SCRIPT_DIR..."
 
 find "$SCRIPT_DIR" -name "*.ts" | while read -r TS_FILE; do
     if [[ "$TS_FILE" == *"Translations"* ]]; then
-        # Use 'sed' to remove the "/Resources/Translations/" part from the file name.
-        # The 's' command stands for substitute. The pipes '|' are used as delimiters
-        # instead of the more common '/' to avoid escaping the slashes in the path.
-        PROJECT_PATH=$(echo $(dirname "$TS_FILE") | sed 's|/Resource/Translations||')
-    
-        lupdate $PROJECT_PATH -ts "$TS_FILE"
+        # Check if TS_LANG is not set or if the current TS file matches the language.
+        if [ -z "$TS_LANG" ] || [[ "$TS_FILE" == *"$TS_LANG"* ]]; then
+            # Use 'sed' to remove the "/Resources/Translations/" part from the file name.
+            # The 's' command stands for substitute. The pipes '|' are used as delimiters
+            # instead of the more common '/' to avoid escaping the slashes in the path.
+            PROJECT_PATH=$(echo $(dirname "$TS_FILE") | sed 's|/Resource/Translations||')
+        
+            lupdate $PROJECT_PATH -ts "$TS_FILE"
+        fi
     fi
 done
