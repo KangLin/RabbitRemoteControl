ac7374d6acf6608baf4533364ba5e6c3d907d62d
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2025-08-31 21:19:29 -0300

    Update PT-BR translation

--- Plugins/TigerVnc/Service/Resource/Translations/TigerVNC_pt_BR.ts
+++ Plugins/TigerVnc/Service/Resource/Translations/TigerVNC_pt_BR.ts
@@ -6,42 +6,42 @@
     <message>
         <location filename="../../FrmParameterTigerVNC.ui" line="14"/>
         <source>Set TigerVNC parameters</source>
-        <translation type="unfinished"></translation>
+        <translation>Configurar parâmetros do TigerVNC</translation>
     </message>
     <message>
         <location filename="../../FrmParameterTigerVNC.ui" line="24"/>
         <source>Base</source>
-        <translation type="unfinished"></translation>
+        <translation>Geral</translation>
     </message>
     <message>
         <location filename="../../FrmParameterTigerVNC.ui" line="30"/>
         <source>Enable</source>
-        <translation type="unfinished"></translation>
+        <translation>Habilitado</translation>
     </message>
     <message>
         <location filename="../../FrmParameterTigerVNC.ui" line="89"/>
         <source>Channel type</source>
-        <translation type="unfinished"></translation>
+        <translation>Tipo de canal</translation>
     </message>
     <message>
         <location filename="../../FrmParameterTigerVNC.ui" line="95"/>
         <source>Enable socket</source>
-        <translation type="unfinished"></translation>
+        <translation>Habilitar socket</translation>
     </message>
     <message>
         <location filename="../../FrmParameterTigerVNC.ui" line="102"/>
         <source>Enable ICE</source>
-        <translation type="unfinished"></translation>
+        <translation>Habilitar ICE</translation>
     </message>
     <message>
         <location filename="../../FrmParameterTigerVNC.ui" line="39"/>
         <source>Port:</source>
-        <translation type="unfinished"></translation>
+        <translation>Porta:</translation>
     </message>
     <message>
         <location filename="../../FrmParameterTigerVNC.ui" line="63"/>
         <source>Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Senha:</translation>
     </message>
 </context>
 <context>
@@ -49,17 +49,17 @@
     <message>
         <location filename="../../PluginServiceTigerVNC.cpp" line="46"/>
         <source>TigerVNC</source>
-        <translation type="unfinished"></translation>
+        <translation>TigerVNC</translation>
     </message>
     <message>
         <location filename="../../PluginServiceTigerVNC.cpp" line="51"/>
         <source>VNC(Virtual Network Console): Access remote desktops such as unix/linux, windows, etc.</source>
-        <translation type="unfinished"></translation>
+        <translation>VNC (Virtual Network Console): Acesse áreas de trabalho remotas, como Unix/Linux, Windows etc.</translation>
     </message>
     <message>
         <location filename="../../PluginServiceTigerVNC.cpp" line="52"/>
         <source>VNC is a set of programs using the RFB (Remote Frame Buffer) protocol.</source>
-        <translation type="unfinished"></translation>
+        <translation>VNC é um conjunto de programas que usam o protocolo RFB (Remote Frame Buffer).</translation>
     </message>
 </context>
 <context>
@@ -67,7 +67,7 @@
     <message>
         <location filename="../../ServiceTigerVNC.cpp" line="156"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Sair</translation>
     </message>
 </context>
 </TS>
